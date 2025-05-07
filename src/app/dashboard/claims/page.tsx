80b3b8f889605e8947c7a9650cf4870b26886731
medinajulioc <138115493+medinajulioc@users.noreply.github.com>
2025-05-07 06:29:18 -0500

    Remove all files except README.md and .gitignore

--- src/app/dashboard/claims/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProjectList from "@/components/ProjectList";
-
-export default function ClaimsPage() {
-  return (
-    <div className="h-full bg-black">
-      <ProjectList />
-    </div>
-  );
-} 
